810d2fa81664fac7984cd005a0c82701c9f97230
Olga <olgapasibayeva98@gmail.com>
2017-12-09 10:58:04 +0200

    Homework 1.30

---
+++ hw_1.30.py
@@ -0,0 +1,18 @@
+#Написать функцию, возвращающую все простые числа от 1 до 100.
+
+
+def gen_primes():
+    lst = []
+    for i in range(1, 101):
+        if i == 1:
+            pass
+        else:
+            for j in lst:
+                if i % j == 0:
+                    break
+            else:
+                lst.append(i)
+    print(lst)
+
+
+gen_primes()
